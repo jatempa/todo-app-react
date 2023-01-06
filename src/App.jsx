5237f7a25bb4d600d770d2638e56ff392342b3c2
Jorge Atempa <atempaitm@gmail.com>
2023-01-06 09:56:52 -0800

    Add new item and clear input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import Card from './components/Card';
+import CustomTaskInput from './components/CustomTaskInput';
 import Header from './components/Header';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [items, setItems] = useState([]);
+
+  const addTask = (event) => {
+    if (event.key === 'Enter') {
+      if (!task || task.length === 0) return;
+
+      const newItem = {
+        id: uuidv4(),
+        title: task,
+        done: false,
+      };
+
+      setItems([...items, newItem]);
+      setTask('');
+    }
+  };
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+
   return (
-    <div id='app'>
-      <Card className='card'>
-        <Header>To Do</Header>
-      </Card>
-    </div>
+    <Card id='app' className='card'>
+      <Header>To Do</Header>
+      <CustomTaskInput
+        task={task}
+        addTask={addTask}
+        handleChange={handleChange}
+      />
+    </Card>
   );
 }
 
